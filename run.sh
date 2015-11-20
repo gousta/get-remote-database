5487eb87d646161ea751c0b112678c47efc3c059
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2015-11-20 11:36:25 +0200

    Improves import procedure by recreating the dropped schema manually before importing from dump.

--- run.sh
+++ run.sh
@@ -128,6 +128,7 @@ puts "[clr 6 $MSG]"
 
 spawn psql -U $LCPGUSER -d $LCPGDBNAME
 send "DROP SCHEMA IF EXISTS $LCPGSCHEMA CASCADE;\r"
+send "CREATE SCHEMA IF NOT EXISTS $LCPGSCHEMA;\r"
 send "\\i $DOWNLOAD_FOLDER/$FILE \r"
 send "\\q\r"
 interact
