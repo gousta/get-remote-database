10614ab79d97013011fe027730f8b16e84861a8e
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2019-02-01 14:14:29 +0200

    Updated mysql export command

--- lib/export_mysql.tcl
+++ lib/export_mysql.tcl
@@ -6,7 +6,7 @@ set MSG "\nExporting"
 puts "[clr 6 $MSG]"
 
 interact -o -nobuffer -re $SSHPROMPT return
-send "mysqldump --opt --protocol=TCP --host=$DBHOST --user=$DBUSER --password=$DBPASSWORD $DBNAME > $EXPORT_FOLDER$FILE\r"
+send "mysqldump --compact --opt --host=$DBHOST --user=$DBUSER --password=$DBPASSWORD $DBNAME > $EXPORT_FOLDER$FILE\r"
 
 send "exit\r"
 interact
