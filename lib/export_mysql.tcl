7faeebba58937bfe206d4992dcef1f5abc097236
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2019-02-01 16:01:35 +0200

    Removed --compact from mysqldump

--- lib/export_mysql.tcl
+++ lib/export_mysql.tcl
@@ -6,7 +6,7 @@ set MSG "\nExporting"
 puts "[clr 6 $MSG]"
 
 interact -o -nobuffer -re $SSHPROMPT return
-send "mysqldump --compact --opt --host=$DBHOST --user=$DBUSER --password=$DBPASSWORD $DBNAME > /tmp/$FILE\r"
+send "mysqldump --opt --host=$DBHOST --user=$DBUSER --password=$DBPASSWORD $DBNAME > /tmp/$FILE\r"
 
 send "exit\r"
 interact
